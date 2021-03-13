c5928407f395f567a389510104a7380c433ea58e
ibohan00 <80597301+ibohan00@users.noreply.github.com>
2021-03-13 10:52:58 -0800

    Add files via upload

---
+++ SerialSend.ino
@@ -0,0 +1,30 @@
+#include <Encoder.h>
+
+// Change these pin numbers to the pins connected to your encoder.
+//   Best Performance: both pins have interrupt capability
+//   Good Performance: only the first pin has interrupt capability
+//   Low Performance:  neither pin has interrupt capability
+Encoder knobLeft(4, 5);
+Encoder knobRight(7, 8);
+//   avoid using pins with LEDs attached
+
+void setup() {
+  Serial.begin(115200);
+}
+
+long positionLeft  = -999;
+long positionRight = -999;
+
+void loop() {
+  long newLeft, newRight;
+  newLeft = knobLeft.read();
+  newRight = knobRight.read();
+    Serial.print("x");
+    Serial.println(newLeft);
+    Serial.print("y");
+    Serial.println(newRight);
+    positionLeft = newLeft;
+    positionRight = newRight;
+    
+    delay(150);
+}
